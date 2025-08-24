62124d7f0c447da8207376a1020c7bc699a6c278
anuarbekadam0101-eng <anuarbek.adam0101@gmail.com>
2025-08-24 19:46:18 +0500

    Update app.py

--- app.py
+++ app.py
@@ -22,6 +22,8 @@ def add():
         'date': date.strftime("%Y-%m-%d %H:%M")
     })
     return redirect(url_for('index'))
+import os
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
